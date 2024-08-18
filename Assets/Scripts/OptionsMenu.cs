f62dee02d23ab5fe5b44372fd0986d39b254b949
VazeMiester <149853708+VazeMiester@users.noreply.github.com>
2024-08-18 18:05:54 -0400

    JOSHO - main and pause menu + audio mixers

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    public AudioMixer MasterMixer;
+    public AudioMixer MusicMixer;
+    public AudioMixer FXMixer;
+    public Slider MasterSlider;
+    public Slider MusicSlider;
+    public Slider FXSlider;
+
+
+    public void SetMasterVol(float MasterVol)
+    {
+        MasterMixer.SetFloat("MasterVol", MasterVol);
+    }
+    public void SetMusicVol(float MusicVol)
+    {
+        MusicMixer.SetFloat("MusicVol", MusicVol);
+    }
+    public void SetFXVol(float FXVol)
+    {
+        FXMixer.SetFloat("FXVol", FXVol);
+    }
+
+    void OnEnable()
+    {
+        if (MasterMixer.GetFloat("MasterVol", out float mastervol))
+        {
+            MasterSlider.value = mastervol;
+        }
+
+        if (MusicMixer.GetFloat("MusicVol", out float musicvol))
+        {
+            MusicSlider.value = musicvol;
+        }
+
+        if (FXMixer.GetFloat("FXVol", out float fxvol))
+        {
+            FXSlider.value = fxvol;
+        }
+
+    }
+
+}
